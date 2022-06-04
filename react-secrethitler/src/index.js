a5d58b90ebd77e4497dd46ae33f9d2dcfb36c377
Tan, Xiu Q <tan.xiu.qi.11@gmail.com>
2022-06-04 16:49:42 +0800

    prepare for next module

--- react-secrethitler/src/index.js
+++ react-secrethitler/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Game from './components/Gamer.js';
+import Game from './components/Gamer2.js';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
